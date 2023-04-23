e15c0e864e345f7cef88a464206285a496d2161b
hunghg255 <giahunghust@gmail.com>
2023-04-23 17:23:22 +0700

    init

---
+++ src/extension.ts
@@ -0,0 +1,140 @@
+import * as vscode from 'vscode';
+
+import * as fs from 'fs';
+import * as path from 'path';
+
+const extensionArray: string[] = ['htm', 'html', 'jsx', 'tsx', 'js'];
+const htmMatchRegex = /class=["'][\w- ]+["']/g;
+const reactMatchRegex =
+  /className=[',",{,\`][a-zA-Z0-9:;.\s\(\)\-,\_'"\+\{\[\}\]\`\$\?]*[',",},\`]/g;
+const fileSep = path.sep;
+
+const regWords = /(classNames|sx|cls|clx|true|false|styles)/g;
+
+const onReplace = (className: string) => {
+  let classNameFormat = className.replace(/styles\./g, ' ');
+  classNameFormat = className.replace(/['"\{\[\]\}\.\,\:\$\`\(\)\+\?]/g, ' ');
+
+  return classNameFormat
+    .split(' ')
+    .filter(Boolean)
+    .map((v) => v.trim());
+};
+
+function provideCompletionItems(
+  document: vscode.TextDocument,
+  position: vscode.Position
+) {
+  const typeText = document
+    .lineAt(position)
+    .text.substring(position.character - 1, position.character);
+
+  if (typeText !== '.') {
+    return;
+  }
+
+  // current file path
+  const filePath: string = document.fileName;
+
+  let classNames: string[] = [];
+  // vue
+  if (document.languageId === 'vue') {
+    classNames = getClass(filePath);
+  }
+  // css-like file
+  else {
+    // current dir path
+    const dir: string = filePath.slice(0, filePath.lastIndexOf(fileSep));
+    // current dir files
+    const files: string[] = fs.readdirSync(dir);
+    // filter target file
+    const target: string[] = files.filter((item: string) =>
+      extensionArray.includes(item.split('.')[1])
+    );
+    // get target files class name
+    target.forEach((item: string) => {
+      const filePath = `${dir}${fileSep}${item}`;
+      const fileClass = getClass(filePath);
+      classNames = classNames.concat(fileClass);
+    });
+  }
+
+  classNames = classNames.reduce((arr, ele) => {
+    const className: string = ele.split('=')[1];
+
+    const field: string[] = onReplace(className);
+
+    arr.push(...field);
+
+    return arr;
+  }, [] as string[]);
+
+  // de-duplication
+  classNames = [...new Set(classNames)];
+
+  return classNames
+    .filter((v) => !regWords.test(v))
+    .map((ele: string) => {
+      const snippetCompletion = new vscode.CompletionItem(
+        document.languageId === 'vue' ? `${ele} ` : `.${ele} `,
+        vscode.CompletionItemKind.Text
+      );
+      snippetCompletion.insertText = new vscode.SnippetString(`
+.${ele} {
+	${`\${0}`}
+}`);
+
+      return snippetCompletion;
+    });
+}
+
+function getClass(path: string) {
+  const data: string = fs.readFileSync(path, 'utf8').split('\n').join('');
+
+  let result;
+  // htm/html/vue use class
+  if (path.includes('htm') || path.includes('vue')) {
+    result = data.match(htmMatchRegex);
+  }
+
+  // tsx/jsx use className
+  if (path.includes('sx') || path.includes('js')) {
+    result = data.match(reactMatchRegex);
+  }
+
+  return result || [];
+}
+
+/**
+ * @param {*} item
+ * @param {*} token
+ */
+function resolveCompletionItem() {
+  return null;
+}
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+  // trigger only when type dot
+  const disposable = vscode.languages.registerCompletionItemProvider(
+    [
+      { scheme: 'file', language: 'css' },
+      { scheme: 'file', language: 'less' },
+      { scheme: 'file', language: 'scss' },
+      { scheme: 'file', language: 'sass' },
+      { scheme: 'file', language: 'stylus' },
+      { scheme: 'file', language: 'vue' },
+    ],
+    {
+      provideCompletionItems,
+      resolveCompletionItem,
+    },
+    '.'
+  );
+
+  context.subscriptions.push(disposable);
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
